31a1c9e5cd78dc063e6006d2fe25c30b318cecbd
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-19 17:32:25 -0400

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,3 +1,5 @@
+const router = require('express').Router();
+
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 
 
@@ -25,4 +27,4 @@
  */
 
 
-// Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
